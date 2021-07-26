44398d9fb05176582598c8f632a964f853d14fd5
odinn1984 <levan@sleep-coding.com>
2021-07-26 20:07:41 +0300

    feat: implement delete endpoints

--- delete.go
+++ delete.go
@@ -1,4 +1,31 @@
 package sensibo
 
-// func (s *Sensibo) DeleteDeviceTimer(id string)                                 {}
-// func (s *Sensibo) DeleteDeviceSchedule(deviceID string, scheduleID string) {}
+import (
+	"fmt"
+)
+
+func (s *Sensibo) DeleteDeviceTimer(id string) (string, error) {
+	resp, err := s.makeDeleteRequest(
+		"v1",
+		fmt.Sprintf("pods/%s/time", id),
+	)
+
+	if err != nil {
+		return "", fmt.Errorf("failed deleting timer: \n\t%v", err)
+	}
+
+	return resp, nil
+}
+
+func (s *Sensibo) DeleteDeviceSchedule(deviceID string, scheduleID string) (string, error) {
+	resp, err := s.makeDeleteRequest(
+		"v1",
+		fmt.Sprintf("pods/%s/schedules/%s", deviceID, scheduleID),
+	)
+
+	if err != nil {
+		return "", fmt.Errorf("failed deleting schedule: \n\t%v", err)
+	}
+
+	return resp, nil
+}
